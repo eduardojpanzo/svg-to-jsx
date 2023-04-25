2428027effffb8abbf44621043a42af910380a11
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-04-25 16:16:34 +0100

    feat: footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <div className="bg-slate-800 text-stone-50 p-4">
+      <p className="text-center font-bold">Developed with by EduardoJPanzo</p>
+    </div>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
+import { Content } from "../components/Content";
+import { Footer } from "../components/Footer";
 import { Navbar } from "../components/Navbar";
 
 export default function Home() {
   return (
     <div className="flex flex-col h-screen">
       <Navbar />
+      <Content />
+      <Footer />
     </div>
   );
 }
