c6aba8af49613d2277e9d4879dcebb8976966736
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2023-05-30 11:54:45 +0100

    improve navbar

---
+++ src/components/Logo.tsx
@@ -0,0 +1,8 @@
+export function Logo() {
+  return (
+    <h1 className="uppercase font-bold">
+      <span> svg </span> to
+      <em> tsx</em>
+    </h1>
+  );
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,19 @@
+import { Logo } from "./Logo";
+
 export function Navbar() {
   return (
     <header className="text-stone-50 bg-slate-800">
       <nav className="flex justify-between py-4 items-center max-w-[1440px] px-8 mx-auto">
-        <a href="#">
-          <img src="/Logo.svg" alt="logo" width="70%" height="70%" />
+        <a href="/">
+          <Logo />
         </a>
 
-        <a href="#">
-          <img src="/Github.svg" alt="GitHub" width="70%" height="70%" />
+        <a
+          className="w-10"
+          href="https://github.com/EduardoPanzo/svg-to-jsx"
+          target="_blanck"
+        >
+          <img src="/Github.svg" alt="GitHub" width="100%" height="100%" />
         </a>
       </nav>
     </header>
